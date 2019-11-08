b0647293627a2f64e8e99f51bd610fa8e868e050
sist <sist@DESKTOP-9IPJ4MD>
2019-11-08 17:41:22 +0900

    유진 - 캘린더

---
+++ academy/src/com/schedule/ScheduleDTO.java
@@ -0,0 +1,59 @@
+package com.schedule;
+
+public class ScheduleDTO {
+	private int num;
+	private String userId, userName;
+	private String lecName, memo;
+	private String sday, eday;
+	private String created;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getLecName() {
+		return lecName;
+	}
+	public void setLecName(String lecName) {
+		this.lecName = lecName;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public String getSday() {
+		return sday;
+	}
+	public void setSday(String sday) {
+		this.sday = sday;
+	}
+	public String getEday() {
+		return eday;
+	}
+	public void setEday(String eday) {
+		this.eday = eday;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	
+	
+}
